64f78c6c7d9875ec608d5c1f564b2e669a127b31
Emmanuel briand <e_briand@hotmail.com>
2022-04-27 11:45:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rockband',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
